9ab182ee81630a2e06a8314c564de956c059161b
Joonhee Lee <joonheelee@JoonLeeMacbookPro.local>
2019-01-30 02:33:26 +0900

    continuous voice recognition

---
+++ app/src/main/java/com/joonheelee/roadster/MainActivity.java
@@ -0,0 +1,249 @@
+package com.joonheelee.roadster;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.icu.text.UnicodeSetSpanner;
+import android.os.Bundle;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vikramezhil.droidspeech.DroidSpeech;
+import com.vikramezhil.droidspeech.OnDSListener;
+import com.vikramezhil.droidspeech.OnDSPermissionsListener;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+
+
+
+/**
+ * Droid Speech Example Activity
+ *
+ * @author Vikram Ezhil
+ */
+
+public class MainActivity extends Activity implements OnClickListener, OnDSListener, OnDSPermissionsListener, OnInitListener {
+    public final String TAG = "Activity_DroidSpeech";
+
+    private DroidSpeech droidSpeech;
+    private Button start, stop;
+    TextView finalSpeechResult;
+    private boolean initialized;
+
+    TextToSpeech tts;
+
+    // MARK: Activity Methods
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Setting the layout;[.
+        setContentView(R.layout.activity_main);
+
+        // Initializing the droid speech and setting the listener
+        droidSpeech = new DroidSpeech(this, getFragmentManager());
+        droidSpeech.setOnDroidSpeechListener(this);
+        droidSpeech.setShowRecognitionProgressView(true);
+        droidSpeech.setRecognitionProgressMsgColor(Color.WHITE);
+        droidSpeech.setOneStepVerifyConfirmTextColor(Color.WHITE);
+        droidSpeech.setOneStepVerifyRetryTextColor(Color.WHITE);
+        droidSpeech.setPreferredLanguage("ko-KR");
+
+        finalSpeechResult = findViewById(R.id.result);
+
+        tts = new TextToSpeech(this, this);
+
+        start = findViewById(R.id.start);
+        start.setOnClickListener(this);
+
+        stop = findViewById(R.id.close);
+        stop.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        if(stop.getVisibility() == View.VISIBLE)
+        {
+            stop.performClick();
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if(stop.getVisibility() == View.VISIBLE)
+        {
+            stop.performClick();
+        }
+    }
+
+    // MARK: OnClickListener Method
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.start:
+
+                // Starting droid speech
+                droidSpeech.startDroidSpeechRecognition();
+
+                // Setting the view visibilities when droid speech is running
+                start.setVisibility(View.GONE);
+                stop.setVisibility(View.VISIBLE);
+
+                break;
+
+            case R.id.close:
+
+                // Closing droid speech
+                droidSpeech.closeDroidSpeechOperations();
+
+                stop.setVisibility(View.GONE);
+                start.setVisibility(View.VISIBLE);
+
+                break;
+        }
+    }
+
+    // MARK: DroidSpeechListener Methods
+
+    @Override
+    public void onDroidSpeechSupportedLanguages(String currentSpeechLanguage, List<String> supportedSpeechLanguages)
+    {
+       /* Log.i(TAG, "Current speech language = " + currentSpeechLanguage);
+        Log.i(TAG, "Supported speech languages = " + supportedSpeechLanguages.toString());
+
+        if(supportedSpeechLanguages.contains("en-US"))
+        {
+            // Setting the droid speech preferred language as tamil if found
+            droidSpeech.setPreferredLanguage("en-US");
+
+            // Setting the confirm and retry text in tamil
+            droidSpeech.setOneStepVerifyConfirmText("confirm");
+            droidSpeech.setOneStepVerifyRetryText("Retry");
+        }*/
+    }
+
+    @Override
+    public void onDroidSpeechRmsChanged(float rmsChangedValue)
+    {
+        // Log.i(TAG, "Rms change value = " + rmsChangedValue);
+    }
+
+    public void callname(){
+
+        String myText1 = "네....";
+        String myText2 = "말씀하세요";
+
+        tts.speak(myText1, TextToSpeech.QUEUE_FLUSH, null);
+        tts.speak(myText2, TextToSpeech.QUEUE_ADD, null);
+
+    }
+
+    @Override
+    public void onDroidSpeechLiveResult(String liveSpeechResult)
+    {
+        Log.i(TAG, "Live speech result = " + liveSpeechResult);
+        if(liveSpeechResult.contains("마크")){
+            droidSpeech.closeDroidSpeechOperations();
+            callname();
+        }
+
+    }
+
+    @Override
+    public void onDroidSpeechFinalResult(String finalSpeechResult) {
+        // Setting the final speech result
+
+        if (finalSpeechResult.equals("마크")) {
+            droidSpeech.closeDroidSpeechOperations();
+            callname();
+        }
+        else
+            droidSpeech.startDroidSpeechRecognition();
+    }
+
+    @Override
+    public void onDroidSpeechClosedByUser()
+    {
+        stop.setVisibility(View.GONE);
+        start.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDroidSpeechError(String errorMsg)
+    {
+        // Speech error
+        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
+
+        stop.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Stop listening
+                stop.performClick();
+            }
+        });
+    }
+
+    // MARK: DroidSpeechPermissionsListener Method
+
+    @Override
+    public void onDroidSpeechAudioPermissionStatus(boolean audioPermissionGiven, String errorMsgIfAny)
+    {
+        if(audioPermissionGiven)
+        {
+            start.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    // Start listening
+                    start.performClick();
+                }
+            });
+        }
+        else
+        {
+            if(errorMsgIfAny != null)
+            {
+                // Permissions error
+                Toast.makeText(this, errorMsgIfAny, Toast.LENGTH_LONG).show();
+            }
+
+            stop.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    // Stop listening
+                    stop.performClick();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+    }
+
+}
